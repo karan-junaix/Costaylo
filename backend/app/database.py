7f54882bbf39e4298eabca4a167974646ffae708
suresh-80553 <suresh-80553@gmail.com>
2026-03-31 10:51:14 +0530

    first commit

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-#DATABASE_URL = "postgresql://postgres:suresh@localhost:5432/pg_listing"
-DATABASE_URL = "postgresql://neondb_owner:npg_zHd5uMBl8ZrO@ep-crimson-credit-a10cuv6h-pooler.ap-southeast-1.aws.neon.tech/Castalyo?sslmode=require&channel_binding=require"
+DATABASE_URL = "postgresql://postgres:suresh@localhost:5432/pg_listing"
+#DATABASE_URL = "postgresql://neondb_owner:npg_zHd5uMBl8ZrO@ep-crimson-credit-a10cuv6h-pooler.ap-southeast-1.aws.neon.tech/Castalyo?sslmode=require&channel_binding=require"
 
 engine = create_engine(DATABASE_URL)
 
